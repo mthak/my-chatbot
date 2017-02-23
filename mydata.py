81f46037f6fed1881e9848ec4b18d2238331cf2c
root <root@cat6ub16.vpc.cloudera.com>
2017-02-22 20:20:59 -0800

    get my first api data

---
+++ mydata.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import json
+import xmltodict
+import json
+import requests
+
+xmldata = requests.get("http://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=MW9S-E7SL-26DU-VV8V")
+
+if xmldata.status_code == 200 :
+   content = xmldata.content
+   mydict = xmltodict(content)
+
+stationinfo = mydict['root']['station']
+
+destinationinfo = mydict['root']['station']['etd']
+
+for key,value in mydict.items():
+ 
+
